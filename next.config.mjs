21f9a42f7f70f1e871984f23cc4ef371c12ba8d0
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-08-17 19:50:18 +0300

    modified the next config

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+import withPlaiceholder from '@plaiceholder/next'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // experimental: {
+    //     serverActions: true,
+    // },
+    images: {
+        domains: [
+            'lh3.googleusercontent.com',
+            'images.unsplash.com',
+            'www.themoviedb.org',
+        ],
+    },
+}
+
+export default withPlaiceholder(nextConfig)
