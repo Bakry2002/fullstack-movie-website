dc50a5eed6f4324f62052ddb2bbc358c07863029
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-08-18 02:12:56 +0300

    Update NavbarLinks.tsx

--- app/components/navbar/NavbarLinks.tsx
+++ app/components/navbar/NavbarLinks.tsx
@@ -2,14 +2,22 @@
 
 import useSearchModal from '@/app/hooks/useSearchModal'
 import MenuItem from './NavLink'
+import { useRouter } from 'next/navigation'
 
 interface NavbarMenuProps {}
 
 const NavbarMenu: React.FC<NavbarMenuProps> = ({}) => {
+    const router = useRouter()
     return (
         <div className="relative">
             <div className="hidden items-center md:flex">
-                <MenuItem label="Home" onClick={() => {}} isNavbarMenu />
+                <MenuItem
+                    label="Home"
+                    onClick={() => {
+                        router.push('/')
+                    }}
+                    isNavbarMenu
+                />
                 <MenuItem label="Movies" onClick={() => {}} isNavbarMenu />
                 <MenuItem label="TV Shows" onClick={() => {}} isNavbarMenu />
                 <MenuItem label="People" onClick={() => {}} isNavbarMenu />
