061d2d668f93244fae52206e4335a6daae5f9819
Abdullah M. Bakry <abdaullah62@gmail.com>
2023-08-18 02:13:02 +0300

    Update Container.tsx

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -2,11 +2,16 @@
 
 interface ContainerProps {
     children: React.ReactNode
+    className?: string
 }
 
-const Container: React.FC<ContainerProps> = ({ children }) => {
+const Container: React.FC<ContainerProps> = ({ children, className }) => {
     return (
-        <div className="mx-auto max-w-[2520px] px-4 sm:px-2 md:px-6 xl:px-10 h-full">
+        <div
+            className={`mx-auto h-full max-w-[2520px] px-4 sm:px-2 md:px-6 xl:px-10 ${
+                className ? className : ''
+            }`}
+        >
             {children}
         </div>
     )
